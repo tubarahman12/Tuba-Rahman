ec0b88ac6c4e662424edf7d645d78e5fd0b31764
tubarahman12 <116910864+tubarahman12@users.noreply.github.com>
2024-08-14 02:46:25 -0700

    sici.c

--- sici.c
+++ sici.c
@@ -1,10 +1,13 @@
 #include<stdio.h>
 #include<math.h>
 int main(){
-	int p,r,t;
+	float p,r,t,n;
 	printf("Enter principal rate and time: ");
-	scanf("%d\n%d\n%d",&p,&r,&t);
-	double si=(p*r*t)/100;
-	double ci=p*pow((1+(r/100)),t);
-	printf("Simple Interst = %f\n Compounbd Interest = %f",si,ci);
+	scanf("%f\n%f\n%f",&p,&r,&t);
+	printf("enter number of times interest is compounded per year= ");
+	scanf("%f",&n);
+	float si=(p*r*t)/100;
+	float amt=p*pow((1+(r/(n*100))),n*t);
+	float ci=amt-p;
+	printf("Simple Interst = %f\nCompound Interest = %f",si,ci);
 }
