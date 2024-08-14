2b2e72b10ce4cc27e84da7783a913d26c6e760c3
tubarahman12 <116910864+tubarahman12@users.noreply.github.com>
2024-08-14 12:33:01 +0530

    insertion.c

--- insertion.c
+++ insertion.c
@@ -13,16 +13,13 @@ int main(){
 	scanf("%d",&num);
 	printf("\nEnter the loacation where you want to insert= ");
 	scanf("%d",&loc);
-	int arr1[n+1];
-	for(int i=0;i<loc;i++){
-		arr1[i]=arr[i];
+	n=n+1;
+	for(int i=n-1;i>=loc;i--){
+		arr[i]=arr[i-1];
 	}
-	arr1[loc]=num;
-	for(int i=loc+1;i<n+1;i++){
-		arr1[i]=arr[i-1];
-	}
-	for(int i=0;i<n+1;i++){
-		printf("%d\t",arr1[i]);
+	arr[pos]=num;
+	for(int i=0;i<n;i++){
+		printf("%d\t",arr[i]);
 	}
 return 0;
 }
